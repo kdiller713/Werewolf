f11f7f4e5e74a07aad444902db0af2b2f9b78eb2
Kyle <kdiller99@gmail.com>
2017-07-22 18:14:09 -0400

    Started writing the server side of the code

---
+++ Werewolf/src/server/text/ServerText.java
@@ -0,0 +1,26 @@
+package server.text;
+
+import java.util.ArrayList;
+
+import listener.ModeratorListener;
+import listener.PlayerListener;
+
+public class ServerText implements PlayerListener {
+    private ArrayList<ModeratorListener> players;
+    private ArrayList<Character> roles;
+    private String[] inputs;
+
+    private void night() {
+
+    }
+
+    private void accuse() {
+
+    }
+
+    @Override
+    public void giveResponse(ModeratorListener ml, String resp) {
+        
+    }
+
+}
