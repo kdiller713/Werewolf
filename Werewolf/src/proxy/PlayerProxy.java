d5b63b0eb77efe8cc62d007193403bc02e1e9658
Kyle <kdiller99@gmail.com>
2017-07-23 16:40:00 -0400

    Added the proxies for the networking to work

---
+++ Werewolf/src/proxy/PlayerProxy.java
@@ -0,0 +1,105 @@
+package proxy;
+
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+import listener.ModeratorListener;
+import listener.PlayerListener;
+
+public class PlayerProxy implements ModeratorListener, Runnable {
+    private Socket sock;
+    private PlayerListener pl;
+    private PrintWriter out;
+    private Scanner sc;
+    private String name;
+
+    public PlayerProxy(Socket s, PlayerListener l, int n, int ind, char role) {
+        sock = s;
+        name = "Player " + (ind + 1);
+        pl = l;
+        try {
+            sc = new Scanner(sock.getInputStream());
+            out.println(role + " " + n + " " + ind);
+        } catch (Exception e) {
+        }
+
+        Thread t = new Thread(this);
+        t.start();
+    }
+
+    @Override
+    public void startNight() {
+        out.println("start");
+    }
+
+    @Override
+    public void awakAtNight() {
+        out.println("awake");
+    }
+
+    @Override
+    public void eliminated() {
+        out.println("elim");
+    }
+
+    @Override
+    public void accuse() {
+        out.println("accuse");
+    }
+
+    @Override
+    public void vote(String defense) {
+        out.println("vote " + defense);
+    }
+
+    @Override
+    public void defend() {
+        out.println("defend");
+    }
+
+    @Override
+    public void defenseBeingMade(String name) {
+        out.println("defense " + name);
+    }
+
+    @Override
+    public void noAccuse() {
+        out.println("no");
+    }
+
+    @Override
+    public void killed(String name) {
+        out.println("killed " + name);
+    }
+
+    @Override
+    public void accussedResult(boolean b) {
+        out.println("result " + b);
+    }
+
+    @Override
+    public void winner() {
+        out.println("winner");
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public void noEat() {
+        out.println("eat");
+    }
+
+    @Override
+    public void run() {
+        try {
+            while (sc.hasNextLine())
+                pl.giveResponse(this, sc.nextLine());
+        } catch (Exception e) {
+
+        }
+    }
+}
---
+++ Werewolf/src/proxy/ServerProxy.java
@@ -0,0 +1,86 @@
+package proxy;
+
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+import listener.ModeratorListener;
+import listener.PlayerListener;
+
+public class ServerProxy implements PlayerListener, Runnable {
+    private ModeratorListener ml;
+    private Socket sock;
+    private PrintWriter out;
+    private Scanner sc;
+
+    public ServerProxy(Socket s, Scanner scan, ModeratorListener p) {
+        sock = s;
+        ml = p;
+        sc = scan;
+        try {
+            out = new PrintWriter(sock.getOutputStream());
+        } catch (Exception e) {
+        }
+
+        Thread t = new Thread(this);
+        t.start();
+    }
+
+    @Override
+    public void giveResponse(ModeratorListener ml, String resp) {
+        out.println(resp);
+    }
+
+    @Override
+    public void run() {
+        try {
+            String line;
+            while (sc.hasNextLine()) {
+                line = sc.nextLine();
+                String[] choices = line.split(" ");
+
+                switch (choices[0]) {
+                case "start":
+                    ml.startNight();
+                    break;
+                case "awake":
+                    ml.awakAtNight();
+                    break;
+                case "elim":
+                    ml.eliminated();
+                    sock.close();
+                    break;
+                case "accuse":
+                    ml.accuse();
+                    break;
+                case "vote":
+                    ml.vote(line.substring(5));
+                    break;
+                case "defend":
+                    ml.defend();
+                    break;
+                case "defense":
+                    ml.defenseBeingMade(line.substring(8));
+                    break;
+                case "no":
+                    ml.noAccuse();
+                    break;
+                case "killed":
+                    ml.killed(line.substring(7));
+                    break;
+                case "result":
+                    ml.accussedResult(Boolean.parseBoolean(choices[1]));
+                    break;
+                case "winner":
+                    ml.winner();
+                    break;
+                case "eat":
+                    ml.noEat();
+                    break;
+                }
+            }
+        } catch (Exception e) {
+
+        }
+    }
+}
